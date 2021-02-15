675a5460c507a34b0d45b4914c62507d95fead9d
ligwol <yulia-aur@mail.ru>
2021-02-15 14:44:26 +0300

    new design, NY+Lnd

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,48 +1,86 @@
 "use strict";
 
-var clock = document.querySelector('#clock-1');
-
-function WriteTime() {
-  var now = new Date();
-  var h = now.getHours();
-  var m = now.getMinutes();
-  var s = now.getSeconds();
-  h = h < 10 ? '0' + h : h;
-  m = m < 10 ? '0' + m : m;
-  s = s < 10 ? '0' + s : s;
-  clock.textContent = "".concat(h, " : ").concat(m, " : ").concat(s);
-  setTimeout(WriteTime, 500);
-}
-
-WriteTime();
-var hoursHand = document.querySelector('.clock-h');
-var minutesHand = document.querySelector('.clock-m');
-var secondsHand = document.querySelector('.clock-s');
-var monthDate = document.querySelector('.clock-md');
-var day = document.querySelector('.clock-d'); // let monthsArray = new Array();
-// let DaysArray = new Array();
-
+var clockNY = document.querySelector('#clock-NY__digital');
+var hoursHandNY = document.querySelector('.clock-NY-h');
+var minutesHandNY = document.querySelector('.clock-NY-m');
+var secondsHandNY = document.querySelector('.clock-NY-s');
+var monthDateNY = document.querySelector('.clock-NY-md');
+var dayNY = document.querySelector('.clock-NY-d');
+var clockLnd = document.querySelector('#clock-Lnd__digital');
+var hoursHandLnd = document.querySelector('.clock-Lnd-h');
+var minutesHandLnd = document.querySelector('.clock-Lnd-m');
+var secondsHandLnd = document.querySelector('.clock-Lnd-s');
+var monthDateLnd = document.querySelector('.clock-Lnd-md');
+var dayLnd = document.querySelector('.clock-Lnd-d');
 var monthsArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 var DaysArray = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-function Clock() {
+function WriteTimeNY() {
   var now = new Date();
-  var h = now.getHours();
-  var m = now.getMinutes();
-  var s = now.getSeconds();
-  var mth = now.getMonth();
-  var d = now.getDate();
-  var dy = now.getDay();
-  console.log(now);
+  var h = now.getUTCHours() - 5;
+  var m = now.getUTCMinutes();
+  var s = now.getUTCSeconds();
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+  clockNY.textContent = "".concat(h, " : ").concat(m, " : ").concat(s);
+  setTimeout(WriteTimeNY, 500);
+}
+
+WriteTimeNY();
+
+function ClockNY() {
+  var now = new Date();
+  var h = now.getUTCHours() - 5;
+  var m = now.getUTCMinutes();
+  var s = now.getUTCSeconds();
+  var mth = now.getUTCMonth();
+  var dt = now.getUTCDate();
+  var dy = now.getUTCDay();
   var sAngle = s * 6;
   var mAngle = m * 6;
   var hAngle = h * 30 + m * 0.5;
-  secondsHand.style.transform = "rotate(".concat(sAngle, "deg)");
-  minutesHand.style.transform = "rotate(".concat(mAngle, "deg)");
-  hoursHand.style.transform = "rotate(".concat(hAngle, "deg)");
-  monthDate.textContent = "".concat(monthsArray[mth], " ").concat(d);
-  day.textContent = "".concat(DaysArray[dy]);
-  setTimeout(Clock, 500);
+  secondsHandNY.style.transform = "rotate(".concat(sAngle, "deg)");
+  minutesHandNY.style.transform = "rotate(".concat(mAngle, "deg)");
+  hoursHandNY.style.transform = "rotate(".concat(hAngle, "deg)");
+  monthDateNY.textContent = "".concat(monthsArray[mth], " ").concat(dt);
+  dayNY.textContent = "".concat(DaysArray[dy]);
+  setTimeout(ClockNY, 500);
 }
 
-Clock();
+ClockNY();
+
+function WriteTimeLnd() {
+  var now = new Date();
+  var h = now.getUTCHours();
+  var m = now.getUTCMinutes();
+  var s = now.getUTCSeconds();
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+  clockLnd.textContent = "".concat(h, " : ").concat(m, " : ").concat(s);
+  setTimeout(WriteTimeLnd, 500);
+}
+
+WriteTimeLnd();
+
+function ClockLnd() {
+  var now = new Date();
+  var h = now.getUTCHours();
+  var m = now.getUTCMinutes();
+  var s = now.getUTCSeconds();
+  var mth = now.getUTCMonth();
+  var dt = now.getUTCDate();
+  var dy = now.getUTCDay();
+  var sAngle = s * 6;
+  var mAngle = m * 6;
+  var hAngle = h * 30 + m * 0.5;
+  secondsHandLnd.style.transform = "rotate(".concat(sAngle, "deg)");
+  minutesHandLnd.style.transform = "rotate(".concat(mAngle, "deg)");
+  hoursHandLnd.style.transform = "rotate(".concat(hAngle, "deg)");
+  monthDateLnd.textContent = "".concat(monthsArray[mth], " ").concat(dt);
+  dayLnd.textContent = "".concat(DaysArray[dy]);
+  setTimeout(ClockLnd, 500);
+}
+
+ClockLnd();
--- script.js
+++ script.js
@@ -1,54 +1,96 @@
-const clock = document.querySelector('#clock-1');
+const clockNY = document.querySelector('#clock-NY__digital');
+const hoursHandNY = document.querySelector('.clock-NY-h');
+const minutesHandNY = document.querySelector('.clock-NY-m');
+const secondsHandNY = document.querySelector('.clock-NY-s');
+const monthDateNY = document.querySelector('.clock-NY-md');
+const dayNY = document.querySelector('.clock-NY-d');
 
-function WriteTime() {
-    const now = new Date();
-    let h = now.getHours();
-    let m = now.getMinutes();
-    let s = now.getSeconds();
-    h = (h < 10) ? '0' + h: h;
-    m = (m < 10) ? '0' + m: m;
-    s = (s < 10) ? '0' + s: s;
-    clock.textContent = `${h} : ${m} : ${s}`;
-    setTimeout(WriteTime, 500);
-}
+const clockLnd = document.querySelector('#clock-Lnd__digital');
+const hoursHandLnd = document.querySelector('.clock-Lnd-h');
+const minutesHandLnd = document.querySelector('.clock-Lnd-m');
+const secondsHandLnd = document.querySelector('.clock-Lnd-s');
+const monthDateLnd = document.querySelector('.clock-Lnd-md');
+const dayLnd = document.querySelector('.clock-Lnd-d');
 
-WriteTime();
 
-const hoursHand = document.querySelector('.clock-h');
-const minutesHand = document.querySelector('.clock-m');
-const secondsHand = document.querySelector('.clock-s');
-const monthDate = document.querySelector('.clock-md');
-const day = document.querySelector('.clock-d');
 
-// let monthsArray = new Array();
-// let DaysArray = new Array();
 let monthsArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', ]
-let DaysArray = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat',];
+let DaysArray = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', ];
 
-function Clock() {
+function WriteTimeNY() {
     let now = new Date();
-    let h = now.getHours();
-    let m = now.getMinutes();
-    let s = now.getSeconds();
-    let mth = now.getMonth();
-    let d = now.getDate();
-    let dy = now.getDay();
-    
-    console.log(now);
+    let h = now.getUTCHours()-5;
+    let m = now.getUTCMinutes();
+    let s = now.getUTCSeconds();
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+    clockNY.textContent = `${h} : ${m} : ${s}`;
+    setTimeout(WriteTimeNY, 500);
+}
+WriteTimeNY();
 
 
+
+function ClockNY() {
+    let now = new Date();
+    let h = now.getUTCHours()-5;
+    let m = now.getUTCMinutes();
+    let s = now.getUTCSeconds();
+    let mth = now.getUTCMonth();
+    let dt = now.getUTCDate();
+    let dy = now.getUTCDay();
     let sAngle = s * 6;
     let mAngle = m * 6;
     let hAngle = h * 30 + (m * 0.5);
 
-    secondsHand.style.transform = `rotate(${sAngle}deg)`;
-    minutesHand.style.transform = `rotate(${mAngle}deg)`;
-    hoursHand.style.transform = `rotate(${hAngle}deg)`;
+    secondsHandNY.style.transform = `rotate(${sAngle}deg)`;
+    minutesHandNY.style.transform = `rotate(${mAngle}deg)`;
+    hoursHandNY.style.transform = `rotate(${hAngle}deg)`;
 
-    monthDate.textContent = `${monthsArray[mth]} ${d}`;
-    day.textContent = `${DaysArray[dy]}`;
-    
-    setTimeout(Clock, 500);
+    monthDateNY.textContent = `${monthsArray[mth]} ${dt}`;
+    dayNY.textContent = `${DaysArray[dy]}`;
+
+    setTimeout(ClockNY, 500);
 }
 
-Clock();
+ClockNY();
+
+function WriteTimeLnd() {
+    let now = new Date();
+    let h = now.getUTCHours();
+    let m = now.getUTCMinutes();
+    let s = now.getUTCSeconds();
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+    clockLnd.textContent = `${h} : ${m} : ${s}`;
+    setTimeout(WriteTimeLnd, 500);
+}
+WriteTimeLnd();
+
+
+
+function ClockLnd() {
+    let now = new Date();
+    let h = now.getUTCHours();
+    let m = now.getUTCMinutes();
+    let s = now.getUTCSeconds();
+    let mth = now.getUTCMonth();
+    let dt = now.getUTCDate();
+    let dy = now.getUTCDay();
+    let sAngle = s * 6;
+    let mAngle = m * 6;
+    let hAngle = h * 30 + (m * 0.5);
+
+    secondsHandLnd.style.transform = `rotate(${sAngle}deg)`;
+    minutesHandLnd.style.transform = `rotate(${mAngle}deg)`;
+    hoursHandLnd.style.transform = `rotate(${hAngle}deg)`;
+
+    monthDateLnd.textContent = `${monthsArray[mth]} ${dt}`;
+    dayLnd.textContent = `${DaysArray[dy]}`;
+
+    setTimeout(ClockLnd, 500);
+}
+
+ClockLnd();
